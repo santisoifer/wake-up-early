7fd24f1e919dc70075f2b3ad20d6b40677edc3c7
Santi Soifer <santisoi2008@gmail.com>
2023-03-07 15:42:44 -0300

    Created the start page

---
+++ src/components/App.jsx
@@ -0,0 +1,22 @@
+import Card from "./Card";
+
+function App() {
+  return (
+    <div>
+      <Card 
+      title="Early wake up" 
+      description="Wake up earlier in a week!"
+      objetive="14/3, 8:00"
+      creator="Santi Soifer"
+      />
+      <Card 
+      title="Not a morning person" 
+      description="For people that find dificult waking up in the mornings."
+      objetive="11/3, 7:30"
+      creator="Santi Soifer"
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,16 @@
+function Card(props) {
+    return (
+        <div className="card">
+            <div className="card__container">
+                <h3>{props.title}</h3>
+                <p>{props.description}</p>
+                <p><i class="fa-solid fa-bullseye"></i> {props.objetive}</p>
+            </div>
+            <div className="card__container">
+                <h3>by {props.creator}</h3>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
